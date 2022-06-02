ca1165b631987ed2112afd7ee523f0bccb26d58a
dexeonify <72267349+dexeonify@users.noreply.github.com>
2022-06-02 18:14:50 +0800

    Tweak smooth scroll values

--- misc/user.js
+++ misc/user.js
@@ -1,11 +1,8 @@
 /* Smooth Scroll  */
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 250);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 450);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
 user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 450);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 250);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 100);
 user_pref("mousewheel.acceleration.factor", 6);
 user_pref("mousewheel.acceleration.start", 4);
 user_pref("mousewheel.min_line_scroll_amount", 22);
-user_pref("toolkit.scrollbox.horizontalScrollDistance", 4);
-user_pref("toolkit.scrollbox.verticalScrollDistance", 5);
