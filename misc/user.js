bc175a6cc09a7e975b1cb82363ad8d27b64ba08f
dexeonify <72267349+dexeonify@users.noreply.github.com>
2022-06-29 19:58:08 +0800

    Add `browser.download.open_pdf_attachments_inline`
    A recent Nightly change. I prefer viewing the PDFs directly inside the
    browser if possible.

--- misc/user.js
+++ misc/user.js
@@ -19,3 +19,7 @@ user_pref("browser.tabs.tabMinWidth", 140);
 /* Prevent middle-click (to autoscroll) from deselecting text */
 /* Relevant bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1714810 */
 user_pref("general.autoscroll.prevent_to_collapse_selection_by_middle_mouse_down", true);
+
+/* Make PDFs sent as attachments open directly in Firefox without downloading */
+/* Relevant bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1772569 */
+user_pref("browser.download.open_pdf_attachments_inline", true);
