b3674512625c38454a2127bd7b58212146dc5ec3
dexeonify <72267349+dexeonify@users.noreply.github.com>
2022-07-03 10:27:49 +0800

    Disable pocket

--- misc/user.js
+++ misc/user.js
@@ -23,3 +23,6 @@ user_pref("general.autoscroll.prevent_to_collapse_selection_by_middle_mouse_down
 /* Make PDFs sent as attachments open directly in Firefox without downloading */
 /* Relevant bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1772569 */
 user_pref("browser.download.open_pdf_attachments_inline", true);
+
+/* Disable pocket */
+user_pref("extensions.pocket.enabled", false);
