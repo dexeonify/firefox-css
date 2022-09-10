402536cf8f980e8f493653bd47cd3c1f8bd85fb1
dexeonify <72267349+dexeonify@users.noreply.github.com>
2022-09-10 22:57:42 +0800

    Tweak smooth scrolling config to be snappier

--- misc/user.js
+++ misc/user.js
@@ -1,10 +1,10 @@
 /* Smooth Scroll  */
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 450);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 100);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 750);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 500);
 user_pref("mousewheel.acceleration.factor", 6);
-user_pref("mousewheel.acceleration.start", 4);
+user_pref("mousewheel.acceleration.start", 6);
 user_pref("mousewheel.min_line_scroll_amount", 22);
 
 /* Enable userChrome customisations */
